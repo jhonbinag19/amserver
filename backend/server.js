512622d30d35bd197c93468b07c382d248625e74
Jhon Binag <jhon@digitalvoltage.net>
2025-04-18 18:48:06 +0800

    Update link

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
-import React from 'react'
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
-const server = () => {
-  return (
-    <div>server</div>
-  )
-}
+const app = express();
 
-export default server
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
